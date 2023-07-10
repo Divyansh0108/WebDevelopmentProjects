005f05b0713f18192f482570bca3a2fac261c1de
Divyansh Pandey <113619486+Divyansh0108@users.noreply.github.com>
2023-07-10 21:14:33 +0530

    Uploaded code files for Speedtest project

--- speedtest_script.js
+++ speedtest_script.js
@@ -12,25 +12,25 @@ let totalMbSpeed = 0;
 let numTests = 1;
 let testCompleted = 0;
 
-// Get random image from unsplash.com
+// Just gets a random image from unsplash which we don't usually give a second thought to
 let imageApi = "https://source.unsplash.com/random?topic=nature";
 
-// When image loads
+// This happens when the image is loading
 image.onload = async function () {
     endTime = new Date().getTime();
 
-    // Get image size
+    // This gets an image size from the response header
     await fetch(imageApi).then((response) => {
         imageSize = response.headers.get("content-length");
         calculateSpeed();
     });
 };
 
-// Function to calculate speed
+// this function basically calculates the speed
 function calculateSpeed() {
-    // Time taken in seconds
+    // The total time taken in seconds
     let timeDuration = (endTime - startTime) / 1000;
-    // Total bits
+    // The total time in bits
     let loadedBits = imageSize * 8;
     let speedInBts = loadedBits / timeDuration;
     let speedInKbs = speedInBts / 1024;
@@ -42,32 +42,31 @@ function calculateSpeed() {
 
     testCompleted++;
 
-    // If all tests completed (we get 5 image then calculate average)
     if (testCompleted === numTests) {
         let averageSpeedInBps = (totalBitSpeed / numTests).toFixed(2);
         let averageSpeedInKbps = (totalKbSpeed / numTests).toFixed(2);
         let averageSpeedInMbps = (totalMbSpeed / numTests).toFixed(2);
 
-        // Display average speeds
+        // This displays average speeds
         bitSpeed.innerHTML += `${averageSpeedInBps}`;
         kbSpeed.innerHTML += `${averageSpeedInKbps}`;
         mbSpeed.innerHTML += `${averageSpeedInMbps}`;
         info.innerHTML = "Test Completed!";
     } else {
-        // Run the next test
+        // Runs the test again
         startTime = new Date().getTime();
         image.src = imageApi;
     }
 }
 
-// Initial function to start tests
+// the initial function 
 const init = async () => {
     info.innerHTML = "Testing...";
     startTime = new Date().getTime();
     image.src = imageApi;
 };
 
-// Run tests when window loads
+// the initial function that runs when the window loads
 window.onload = () => {
     for (let i = 0; i < numTests; i++) {
         init();
