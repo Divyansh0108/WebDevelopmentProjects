3d69c84df1d93f6c098ea8d74cb47c98a2dbfbb8
Divyansh Pandey <113619486+Divyansh0108@users.noreply.github.com>
2024-02-02 22:09:05 +0530

    added: symbol selection, saving the names, turn logic and highlighting

---
+++ tic tac toe/src/components/Log.jsx
@@ -0,0 +1,9 @@
+export default function Log(){
+    return <div id="log">
+        <h2>Game Log</h2>
+        <ol id="log-list">
+            <li>Player 1 has made a move</li>
+            <li>Player 2 has made a move</li>
+        </ol>
+    </div>
+}
--- tic tac toe/src/components/Player.jsx
+++ tic tac toe/src/components/Player.jsx
@@ -1,25 +1,36 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}) {
+export default function Player({ initialName, symbol, isActive }) {
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
+    const [editedName, setEditedName] = useState(initialName);
 
     function handleEditClick() {
+        if (isEditing) {
+            setPlayerName(editedName);
+        }
         setIsEditing((editing) => !editing);
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event) {
+        setEditedName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
 
     if (isEditing) {
-        playerName = <input type="text" required value={name} />;
+        editablePlayerName = (
+            <input type="text" required value={editedName} onChange={handleChange} />
+        );
     }
 
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined }>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>{isEditing ? "Save":"Edit"}</button>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     );
 }
